39a6e11306406afd8d76847f4a771aec8d9d1d8f
石于诚 <1695189918@qq.com>
2025-12-24 16:07:02 +0800

    fix: remove comments

--- main.go
+++ main.go
@@ -79,7 +79,6 @@ func startServer(addr chan string) {
 	mux.Handle(server.DefaultDebugPath, server.DebugHTTP{s})
 	addr <- l.Addr().String()
 	// server.DefaultServer.Accept(l)
-	// s.Accept
 	
 	go s.Accept(l)
 	go http.Serve(hl, mux)
@@ -173,32 +172,6 @@ func newBroadcast(registry string) {
 
 func main() {
 	log.SetFlags(0)
-	// addr := make(chan string)
-	// go startServer(addr)
-
-	// // in fact, following code is like a simple geerpc client
-	// client, _ := client.DialHTTP("tcp", <-addr)
-	// // defer func() { _ = client.Close() }()
-
-	// time.Sleep(time.Second)
-
-	// // _ = json.NewEncoder(conn).Encode(server.TestOption)
-
-	// var wg sync.WaitGroup
-	// for i := 0; i < 5; i++ {
-	// 	wg.Add(1)
-	// 	go func(i int) {
-	// 		defer wg.Done()
-	// 		args := &Args{Num1: i, Num2: i * i}
-	// 		ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	// 		var reply int
-	// 		if err := client.Call(ctx, "Foo.Sum", args, &reply); err != nil {
-	// 			log.Fatal("call testing error: ", err)
-	// 		}
-	// 		log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
-	// 	}(i)
-	// }
-	// wg.Wait()
 	registryAddr := "http://localhost:9999/_gorpc_/registry"
 	var wg sync.WaitGroup
 	wg.Add(1)
